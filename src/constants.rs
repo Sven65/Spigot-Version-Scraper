7b042e50aab05284d10c7b7171763cee458b7a70
Sven65 <Sven65@users.noreply.github.com>
2025-10-30 15:05:16 +0100

    Update user agent

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
 
 pub const FORUM_URL: &str = "https://www.spigotmc.org/";
-pub const REQWEST_USER_AGENT: &str = "MyCustomUserAgent/1.0";
+pub const REQWEST_USER_AGENT: &str = "SpigotVersionFetcher/1.0";
